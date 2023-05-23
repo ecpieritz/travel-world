ace75a1969d91f1862ff2f944bd71e75b4a06868
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-05-23 15:50:20 -0300

    Added Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './header/nav/nav.component';
+import { BannerComponent } from './header/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
