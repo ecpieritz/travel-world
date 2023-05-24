0e071214cdcb69c4bb10f532232fbef1c576ca27
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-05-24 09:51:56 -0300

    Added Services base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './header/nav/nav.component';
 import { BannerComponent } from './header/banner/banner.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavComponent,
-    BannerComponent
+    BannerComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule
